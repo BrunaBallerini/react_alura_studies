6c39634129a681e455e98f07c699ff816fc1f443
BrunaBallerini <bruna@webgoal.com.br>
2024-10-29 20:37:58 -0300

    Criando componente item dentro de lista

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,3 +1,4 @@
+import Item from './Item';
 import style from './List.module.scss'
 
 function ListComponent() {
@@ -13,10 +14,10 @@ function ListComponent() {
             <h2>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li className={style.item} key={index}>
-                        <h3>{item.tarefa}</h3>
-                        <span>{item.tempo}</span>
-                    </li>
+                    <Item
+                        key={index}
+                        tarefa={item.tarefa} tempo={item.tempo}
+                    />
                 ))}
             </ul>
         </aside>
